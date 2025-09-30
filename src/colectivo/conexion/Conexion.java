a1633e399dd5df13c74244da81dad68787c5ac91
Jonathan <jonathan1.perez750@gmail.com>
2025-09-30 19:35:40 -0300

    Clase java Conexion

---
+++ src/colectivo/conexion/Conexion.java
@@ -0,0 +1,18 @@
+package colectivo.conexion;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static Connection conn;
+
+    public static Connection getConnection() throws SQLException {
+        if (conn == null || conn.isClosed()) {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tu_base", "usuario", "contraseña");
+        }
+        return conn;
+    }
+}
+
