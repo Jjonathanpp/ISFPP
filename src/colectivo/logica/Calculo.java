053111f0c22621f4247b4a570079ef42fb503728
Jonathan <jonathan1.perez750@gmail.com>
2025-10-01 17:20:43 -0300

    Clase Calculo

---
+++ src/colectivo/logica/Calculo.java
@@ -0,0 +1,21 @@
+package colectivo.logica;
+
+//Modo de prueba
+import colectivo.modelo.Parada;
+import colectivo.modelo.Recorrido;
+import colectivo.modelo.Tramo;
+
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
+
+public class Calculo {
+
+
+
+    //metodo Calcular el recorrido
+    public  List<List<Recorrido>> calcularRecorrido(Parada paradaOrigen, Parada paradaDestino, int diaSemana,
+                                                    LocalTime horaLlegadaParada, Map<String, Tramo> tramos) {
+        return null;
+    }
+}
