13b5c1df34e0bfd3c12c48533a5013aff94ad1c9
Jonathan <jonathan1.perez750@gmail.com>
2025-10-05 12:42:46 -0300

    Clase Calculo

--- src/colectivo/logica/Calculo.java
+++ src/colectivo/logica/Calculo.java
@@ -1,11 +1,13 @@
 package colectivo.logica;
 
 //Modo de prueba
+import colectivo.modelo.Linea;
 import colectivo.modelo.Parada;
 import colectivo.modelo.Recorrido;
 import colectivo.modelo.Tramo;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,6 +18,17 @@ public class Calculo {
     //metodo Calcular el recorrido
     public  List<List<Recorrido>> calcularRecorrido(Parada paradaOrigen, Parada paradaDestino, int diaSemana,
                                                     LocalTime horaLlegadaParada, Map<String, Tramo> tramos) {
+
+      d
+
+        // 1. Consultar los tramos y horarios desde DAO
+        // 2. Buscar rutas directas
+        // 3. Si no hay, buscar combinaciones de líneas
+        // 4. Calcular tiempos y horarios de paso
+        // 5. Retornar lista de recorridos posibles
+
+
         return null;
     }
+
 }
