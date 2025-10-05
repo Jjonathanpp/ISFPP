9e53006a71848c1ea8fde966e300fcd6901c8570
Jonathan <jonathan1.perez750@gmail.com>
2025-10-05 12:44:29 -0300

    Clase LineaDAO

--- src/colectivo/dao/LineaDAO.java
+++ src/colectivo/dao/LineaDAO.java
@@ -1,4 +1,17 @@
 package colectivo.dao;
 
-public class LineaDAO {
+import colectivo.modelo.Linea;
+
+import java.util.Map;
+
+public interface LineaDAO {
+
+
+    void insertar(Linea linea);
+
+    void actualizar(Linea linea);
+
+    void borrar(Linea linea);
+
+    Map<String, Linea> buscarTodos();
 }
