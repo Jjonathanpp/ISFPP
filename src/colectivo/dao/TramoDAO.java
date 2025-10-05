26f0d7817400ba1c96061881fbd7a45f2c151747
Jonathan <jonathan1.perez750@gmail.com>
2025-10-05 12:45:26 -0300

    Clase TramoDAO

--- src/colectivo/dao/TramoDAO.java
+++ src/colectivo/dao/TramoDAO.java
@@ -1,4 +1,16 @@
 package colectivo.dao;
 
-public class TramoDAO {
+import colectivo.modelo.Tramo;
+
+import java.util.Map;
+
+public interface TramoDAO {
+
+    void insertar (Tramo tramo);
+
+    void actualizar (Tramo tramo);
+
+    void borrar(Tramo tramo);
+
+    Map<String, Tramo> buscarTodos();
 }
