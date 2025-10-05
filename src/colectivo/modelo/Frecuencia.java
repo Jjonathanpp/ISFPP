4702628a2a072da29c1e85291e0d00be66133c41
Jonathan <jonathan1.perez750@gmail.com>
2025-10-05 12:44:10 -0300

    Clase Frecuencia

--- src/colectivo/modelo/Frecuencia.java
+++ src/colectivo/modelo/Frecuencia.java
@@ -1,4 +1,44 @@
 package colectivo.modelo;
 
+import java.time.LocalTime;
+
 public class Frecuencia {
+
+    //Atributos
+    private int diaSemana;
+    private LocalTime hora;
+
+    private Linea linea;
+
+    //Constructor
+    public Frecuencia(int diaSemana, LocalTime hora, Linea linea) {
+        this.diaSemana = diaSemana;
+        this.hora = hora;
+        this.linea = linea;
+    }
+
+    //getters y setters
+    public int getDiaSemana() {
+        return diaSemana;
+    }
+
+    public void setDiaSemana(int diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public Linea getLinea() {
+        return linea;
+    }
+
+    public void setLinea(Linea linea) {
+        this.linea = linea;
+    }
 }
