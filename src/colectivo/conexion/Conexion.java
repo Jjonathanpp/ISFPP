1de33289b139b124ea753138e8ad043566171fa8
Jonathan <jonathan1.perez750@gmail.com>
2025-10-05 12:43:57 -0300

    Clase Conexion

--- src/colectivo/conexion/Conexion.java
+++ src/colectivo/conexion/Conexion.java
@@ -8,11 +8,23 @@ import java.sql.SQLException;
 public class Conexion {
     private static Connection conn;
 
+    //para conectar a la base de datos
     public static Connection getConnection() throws SQLException {
         if (conn == null || conn.isClosed()) {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/postgres", "postgres", "dosmil27");
         }
         return conn;
+    }
+
+    // Método para cerrar conexión
+    public static void cerrarConexion(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
