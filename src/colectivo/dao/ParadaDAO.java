18649617427d1bad4e7ea92c85e7ef48a36577e2
Jonathan <jonathan1.perez750@gmail.com>
2025-10-05 12:44:48 -0300

    Clase ParadaDAO

--- src/colectivo/dao/ParadaDAO.java
+++ src/colectivo/dao/ParadaDAO.java
@@ -1,4 +1,16 @@
 package colectivo.dao;
 
-public class ParadaDAO {
+import colectivo.modelo.Parada;
+
+import java.util.Map;
+
+public interface ParadaDAO {
+
+    void insertar(Parada parada);
+
+    void actualizar(Parada parada);
+
+    void borrar(Parada parada);
+
+    Map<Integer, Parada> buscarTodos();
 }
