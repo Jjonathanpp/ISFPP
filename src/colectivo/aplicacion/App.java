687d2b2584c2eeb28b84aef7ea552199168c76b3
Jonathan <jonathan1.perez750@gmail.com>
2025-10-31 20:59:11 -0300

    nueva clase app

--- src/colectivo/aplicacion/App.java
+++ src/colectivo/aplicacion/App.java
@@ -3,53 +3,34 @@ package colectivo.aplicacion;
 import colectivo.interfaz.Controler;
 import colectivo.interfaz.VistaInterfaz;
 import colectivo.logica.Empresa;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import org.apache.log4j.Logger;
 
-import java.util.ResourceBundle;
+public class App {
 
-public class App extends Application {
+    private final Coordinador coordinador;
+    private final VistaInterfaz vista;
+    private final Controler controlador;
+    private final Configuracion configuracion;
 
-    @Override
-    public void start(Stage stage) {
-        try {
-            // 1) Crear coordinador y cargar datos
-            Coordinador coordinador = new Coordinador();
-            coordinador.setEmpresa(Empresa.getEmpresa()); //Porque usa singleton
+    public App() {
+        coordinador = new Coordinador();
+        coordinador.setEmpresa(Empresa.getEmpresa());
 
-            // 2) Hacer que el coordinador construya (arme) la vista
-            VistaInterfaz vista = coordinador.getVista(); //Aca es donde se crea la vista unicamente para pasarsela al controlador
-            Controler controlador = new Controler(vista, coordinador);
+        vista = coordinador.getVista();
+        controlador = coordinador.getControlerMVC();
 
-            // 4) Crear y configurar el controlador
-            coordinador.construirVistaConControler();
-            controlador.inicializar();
+        coordinador.construirVistaConControler();
+        controlador.inicializar();
 
-            // 5) App gestiona la ventana
-            Configuracion configuracion = Configuracion.getInstance();
-            ResourceBundle bundle = configuracion.getBundle();
-            stage.setTitle(bundle.getString("app.windowTitle"));
-            configuracion.localeProperty().addListener((obs, oldLocale, newLocale) ->
-                    stage.setTitle(configuracion.getBundle().getString("app.windowTitle"))
-            );
-            stage.setScene(new Scene(vista.getRoot(), 520, 600));
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-            //Creo que va el logger aca
-        }
+        configuracion = Configuracion.getInstance();
+    }
+
+    public VistaInterfaz getVista() {
+        return vista;
+
 
     }
 
-    public static void main(String[] args) {
-        //Si se hace un singleton de conexion entonces esto sirve??
-        //ESTO se supone que es el shutdown hook para cerrar la conexion a la BD (no se si habria que hacer algo mas con esto)
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("ShutdownHook: cerrando conexion BD...");
-            try { colectivo.conexion.Conexion.cerrar(); } catch (Throwable t) { t.printStackTrace(); }
-        }));
-        launch(args);
+    public Configuracion getConfiguracion() {
+        return configuracion;
     }
 }
